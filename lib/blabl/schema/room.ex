4785f7938847ed35ccce2278c35b37bc86f7366e
Petr Stepchenko <nitrinodev@gmail.com>
2019-05-12 23:27:45 +0300

    Add room table and schema

---
+++ lib/blabl/schema/room.ex
@@ -0,0 +1,18 @@
+defmodule Blabl.Schema.Room do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "rooms" do
+    field :name, :string
+    field :topic, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(room, attrs) do
+    room
+    |> cast(attrs, [:name, :topic])
+    |> validate_required([:name, :topic])
+  end
+end
---
+++ priv/repo/migrations/20190512202139_create_rooms.exs
@@ -0,0 +1,14 @@
+defmodule Blabl.Repo.Migrations.CreateRooms do
+  use Ecto.Migration
+
+  def change do
+    create table(:rooms) do
+      add :name, :string, null: false
+      add :topic, :string, default: ""
+
+      timestamps()
+    end
+
+    create unique_index(:rooms, [:name])
+  end
+end
