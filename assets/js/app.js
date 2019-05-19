2cd7ef327fb90d6c4b531928c1c05ffbd3e53a70
Petr Stepchenko <nitrinodev@gmail.com>
2019-05-19 10:32:39 +0300

    Add sanitize.css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
+import 'sanitize.css';
+import 'sanitize.css/typography.css';
+import 'sanitize.css/forms.css';
 import css from "../css/app.scss";
 
 // webpack automatically bundles all modules in your
