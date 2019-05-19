69cb49e565bbbc0716057b90f77c7a5cc36dac67
Petr Stepchenko <nitrinodev@gmail.com>
2019-05-19 12:06:20 +0300

    Show active room

--- lib/blabl_web/live/messenger_live.ex
+++ lib/blabl_web/live/messenger_live.ex
@@ -1,8 +1,8 @@
 defmodule BlablWeb.MessengerLive do
   use Phoenix.LiveView
 
-  alias BlablWeb.MessengerView
   alias Blabl.Messenger
+  alias BlablWeb.MessengerView
 
   def render(assigns) do
     MessengerView.render("index.html", assigns)
@@ -10,11 +10,13 @@ defmodule BlablWeb.MessengerLive do
 
   def mount(session, socket) do
     rooms = Messenger.list_rooms(session[:user_id])
-    {:ok, assign(socket, user_id: session[:user_id], rooms: rooms, events: [])}
+    first_romm = rooms |> List.first
+    events = Messenger.list_events(first_romm.id, session[:user_id])
+    {:ok, assign(socket, user_id: session[:user_id], rooms: rooms, events: events, active_room_id: first_romm.id)}
   end
 
   def handle_event("show_room", room_id, socket) do
     events = Messenger.list_events(room_id, socket.assigns.user_id)
-    {:noreply, assign(socket,  events: events)}
+    {:noreply, assign(socket, events: events, active_room_id: String.to_integer(room_id))}
   end
 end
